8b6af54c287dba7fa5b3d425c985ad636c3a257d
Evacur <cuervapbl@gmail.com>
2025-05-17 09:52:59 +0200

    Collaboration

--- schemaTypes/collaboration.ts
+++ schemaTypes/collaboration.ts
@@ -20,6 +20,7 @@ export default defineType({
       title: 'Image de couverture',
       type: 'image',
       options: { hotspot: true },
+      validation: (Rule) => Rule.required(),
     }),
     defineField({
       name: 'title',
