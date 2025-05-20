87f74b793dc284f23b329fb012f8fe790904b18f
Evacur <cuervapbl@gmail.com>
2025-05-20 18:09:20 +0200

    Done

--- sanity.config.js
+++ sanity.config.js
@@ -1,7 +1,7 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
 
 
 export default defineConfig({
@@ -11,7 +11,7 @@ export default defineConfig({
   projectId: 'h3wae5wn',
   dataset: 'production',
 
-  plugins: [visionTool()],
+  plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
