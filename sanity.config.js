dc43c721523aa599a817d8cb684831615cdbaf33
Evacur <cuervapbl@gmail.com>
2025-06-06 09:20:10 +0200

    test

--- sanity.config.js
+++ sanity.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   name: 'default',
   title: 'portfolio',
 
-  projectId: 'h3wae5wn',
+  projectId: '730lhcwc',
   dataset: 'production',
 
   plugins: [structureTool(), visionTool()],
