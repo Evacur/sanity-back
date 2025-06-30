2141ac449d798e2fe6728dc08ebb7f846efe3c99
Evacur <cuervapbl@gmail.com>
2025-06-30 16:58:45 +0200

    Clean

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -2,4 +2,4 @@ import collaboration from './collaboration'
 import creation from './creation'
 import creationSection from './creationSection'
 import exposition from './exposition'
-export const schemaTypes = [creation, creationSection, collaboration, exposition]
+export const schemaTypes = [creationSection, creation, collaboration, exposition]
